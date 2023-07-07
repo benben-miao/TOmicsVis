847880510afc29f8564a82e563be6aa739e5631f
Wei Dong <1369852697@qq.com>
2023-07-07 11:19:53 +0800

    Add new function gene_cluster_trend
    Add new function gene_cluster_trend

---
+++ R/gene_cluster_trend.R
@@ -0,0 +1,76 @@
+#' @title Gene cluster trend plot for visualizing gene expression trend profile in multiple samples.
+#' @description Gene cluster trend plot for visualizing gene expression trend profile in multiple samples.
+#' @author wei dong
+#'
+#' @return Plot: Gene cluster trend plot for visualizing gene expression trend profile in multiple samples.
+#' @param data Dataframe: gene expression dataframe with cols (samples) and rows (genes).
+#' @param thres Number: set the threshold for excluding genes. Default: 0.25.
+#' @param min_std Number: set the threshold for minimum standard deviation. Default: 0.2.
+#' @param palette Character: set the color palette to be used for plotting. Default: "Spectral", options: 'BrBG', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn'.
+#' @param cluster_num Number: set the number of clusters. Default: 6.
+#'
+#' @import Mfuzz
+#' @import RColorBrewer
+#' @export
+#'
+#' @examples
+#' # 1. Library TOmicsVis package
+#' library(TOmicsVis)
+#'
+#' # 2. Use example dataset gene_cluster
+#' data(gene_cluster)
+#' head(gene_cluster)
+#'
+#' # 3. Default parameters
+#' gene_cluster_trend(gene_cluster)
+#'
+#' # 4. Set palette = "RdBu"
+#' gene_cluster_trend(gene_cluster, palette = "RdBu")
+#'
+#' # 5. Set cluster_num = 6
+#' gene_cluster_trend(gene_cluster, cluster_num = 6, palette = "Spectral")
+#'
+#' # 6. Set thres = 0.1
+#' gene_cluster_trend(gene_cluster, thres = 0.1, palette = "RdYlGn")
+#'
+gene_cluster_trend <- function(data, 
+                               thres = 0.25,
+                               min_std = 0.2,
+                               palette = "PiYG",
+                               cluster_num = 4
+                               ){
+  
+  # create ExpressionSet object
+  eset <- new("ExpressionSet", exprs = as.matrix(data))
+  
+  # Data pre-processing
+  eset <- Mfuzz::filter.NA(eset, thres= thres)
+  #eset <- fill.NA(eset,mode="mean",k=10)
+  eset <- Mfuzz::filter.std(eset, min.std= min_std, visu = F)
+  
+  # data normalization
+  eset <- Mfuzz::standardise(eset)
+  # set cluster number
+  c <- cluster_num
+  # estimate m value
+  m <- Mfuzz::mestimate(eset)
+  
+  # perform mfuzz clustering
+  cl <- Mfuzz::mfuzz(eset, centers  = c, m = m)
+  
+  # set the color palettes
+  # The diverging palettes are: BrBG PiYG PRGn PuOr RdBu RdGy RdYlBu RdYlGn Spectral
+  palettes <- RColorBrewer::brewer.pal(8, palette)
+  colors <- rev(grDevices::colorRampPalette(palettes)(1000))
+  
+  # plot gene cluster trend
+  p <- Mfuzz::mfuzz.plot(
+    eset,
+    cl,
+    mfrow=c(2,(c/2+0.5)),
+    colo = colors,
+    time.labels = colnames(eset),
+    new.window= FALSE)
+  
+  return(p)
+}
