eb4431e465511dfbf4048bb9cb5851c0487241ad
Wei Dong <1369852697@qq.com>
2023-07-18 16:49:35 +0800

    Add example data for heatmap_cluster function
    Add example data for heatmap_cluster function

---
+++ R/gene_exp2.R
@@ -0,0 +1,26 @@
+#' @title Gene Expression dataframe of RNA-Seq.
+#' @description Gene Expression dataframe of RNA-Seq.
+#' @author wei dong
+#'
+#' @docType data
+#' @format Dataframe: gene expression dataframe with cols (samples) and rows (genes).
+#' @usage data(gene_exp2)
+#'
+#' @keywords datasets
+#'
+#' @references https://github.com/BioSciTools/BioSciToolsDatasets/tree/main/CorPlot/
+#'
+#' @examples
+#' # 1. Library TOmicsVis package
+#' library(TOmicsVis)
+#'
+#' # 2. Load example dataset gene_exp2
+#' data(gene_exp2)
+#'
+#' # 3. View gene_exp2
+#' gene_exp2
+#'
+"gene_exp2"
+
+# gene_exp2 <- data
+# usethis::use_data(gene_exp2)
